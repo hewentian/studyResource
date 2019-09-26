87b38642c825cc79df86ed9639286b2e0cf6e8f7
hewentian <wentian.he@qq.com>
2019-09-26 14:32:28 +0800

    移除java-jdk

--- codes/java-jdk/src/main/java/com/hewentian/jdk/c01/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package com.hewentian.jdk.c01;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
--- codes/java-jdk/src/main/java/com/hewentian/jdk/c02/s02/Dog.java
+++
@@ -1,9 +0,0 @@
-package com.hewentian.jdk.c02.s02;
-
-public class Dog {
-    private int weight;
-
-    public Dog(int weight) {
-        this.weight = weight;
-    }
-}
